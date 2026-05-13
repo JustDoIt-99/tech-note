4b57b122c0a7c5cd485c40c6173c525cb3c19b26
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-13 15:34:14 +0900

    algorithm: 영어 끝말잇기

---
+++ docs/algorithm/programmers/level_one/영어끝말잇기/solve.js
@@ -0,0 +1,22 @@
+function solution(n, words) {
+    var answer = [];
+    let wordSet = new Set();
+
+    wordSet.add(words[0]);
+    let order = 0;
+
+    for(let i = 1; i < words.length; i++) { // O(n)
+
+        let bf = words[i - 1].charAt(words[i - 1].length - 1);
+        let af = words[i].charAt(0);
+
+        if(wordSet.has(words[i]) || bf != af || words[i].length == 1) {
+            order = i + 1;
+            return order != 0 ? [order % n == 0 ? n : order % n, Math.ceil(order / n)] : [0,0];
+        }
+
+        wordSet.add(words[i]);
+    }
+
+    return [0,0];
+}
