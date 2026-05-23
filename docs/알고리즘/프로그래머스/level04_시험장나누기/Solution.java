8d8fe85c60de20899bb0c26a54aa358eb806e2d9
JustDoIt-99 <sangyunpark99@gmail.com>
2026-05-23 10:35:48 +0900

    refactor: 알고리즘 프로그래머스 시험장 나누기

--- docs/알고리즘/프로그래머스/level04_시험장나누기/Solution.java
+++ docs/알고리즘/프로그래머스/level04_시험장나누기/Solution.java
@@ -1,40 +1,39 @@
 import java.util.*;
 
 class Solution {
+    // 1. 루트 노드 찾기
+    // 2. 이분 탐색
 
-    private static Set<Integer> rootCheck = new HashSet<>();
-    private static int n;
-    private int groupCount = 0;
-    private int k;
-    private int[] num;
     private int[][] links;
+    private int[] num;
+    private Set<Integer> checkRoot = new HashSet<>();
+    private int k;
+    private int n;
+    private int groupCount;
 
     public int solution(int k, int[] num, int[][] links) {
         int answer = 0;
 
-        this.num = num;
         this.k = k;
         this.links = links;
+        this.num = num;
+        this.n = num.length;
+
+        int root = findRoot();
+
+        return binarySearch(root);
+    }
+
+    private int binarySearch(int root) {
 
         int left = Arrays.stream(num).max().getAsInt();
         int right = Arrays.stream(num).sum();
-        n = num.length;
-
-        for(int i = 0; i < n; i++) {
-            rootCheck.add(i);
-        }
-
-        int root = findRoot();
-        return search(left, right, root);
-    }
-
-    private int search(int left, int right, int root) { // 인원이 가장 많은 그룹의 인원이 최소화
-        int mid = 0;
         int answer = 0;
-        while(left <= right) {
-            mid = (left + right) / 2;
 
-            if(check(mid, root))  {
+        while(left <= right) {
+            int mid = (left + right) / 2;
+
+            if(check(mid, root)) {
                 answer = mid;
                 right = mid - 1;
             }else {
@@ -46,56 +45,59 @@ class Solution {
     }
 
     private boolean check(int limit, int root) {
-        groupCount = 1; // 초기화
-        if(checkDfs(root, limit) == -1) { // -1을 반환하는 경우
-            return false;
-        }
-
+        groupCount = 1;
+        if(dfs(root, limit) == -1) return false;
         return groupCount <= k;
     }
 
-    // 현재 노드 + 왼쪽 서브트리 합 + 오른쪽 서브 트리합
-    private int checkDfs(int cur, int limit) {
+    private int dfs(int cur, int limit) {
+        int total = 0;
+        int left = 0;
+        int right = 0;
 
         if(num[cur] > limit) return -1;
 
-        int left = 0;
-        int right = 0;
         int leftChild = links[cur][0];
         int rightChild = links[cur][1];
 
-        if(leftChild != -1) { // 자식이 있는 경우
-            left += checkDfs(leftChild, limit);
+        if(leftChild != -1) { // 왼쪽 서브 트리
+            left += dfs(leftChild, limit);
             if(left == -1) return -1;
         }
 
-        if(rightChild != -1) {
-            right += checkDfs(rightChild, limit);
+        if(rightChild != -1) { // 오른쪽 서브 트리
+            right += dfs(rightChild, limit);
             if(right == -1) return -1;
         }
 
-        int total = left + right + num[cur]; // 서브 트리의 합
+        total = left + right + num[cur];
 
         if(total <= limit) {
             return total;
         }
 
-        if(num[cur] + Math.min(left, right) <= limit) { // 더 작은 수를 더하기
-            groupCount++; // 그룹 나누기
+        if(num[cur] + Math.min(left, right) <= limit) { // 자식중 큰거 잘라
+            groupCount++;
             return num[cur] + Math.min(left, right);
         }
 
-        groupCount += 2; // 그룹 둘로 나누기
+        groupCount += 2; // 둘다 잘라
         return num[cur];
     }
 
     private int findRoot() {
 
         for(int i = 0; i < n; i++) {
-            if(links[i][0] != -1) rootCheck.remove(links[i][0]);
-            if(links[i][1] != -1) rootCheck.remove(links[i][1]);
+            checkRoot.add(i);
         }
 
-        return rootCheck.iterator().next();
+        for(int i = 0; i < n; i++) {
+            int left = links[i][0];
+            int right = links[i][1];
+            checkRoot.remove(left);
+            checkRoot.remove(right);
+        }
+
+        return checkRoot.iterator().next();
     }
 }
